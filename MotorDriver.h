0f3f37c30e43ba84c2bbf0eae801a67b642cac23
soliedra <mrjaviercasado@yahoo.es>
2016-07-17 20:30:59 +0200

    written some comments

--- MotorDriver.h
+++ MotorDriver.h
@@ -1,3 +1,19 @@
+/*
+ * 
+ * Provides an homogeneous interface to deal with a variety of pwm motor drivers in Arduino.
+ * All PWM motor drivers control the speed and direction of a DC motor using different number of
+ * pins. The aim of this class is to provide an homogeneous interface for an Arduino program to
+ * be able to control the speed and direction of a DC Motor.
+ *
+ * It contains just one method called writePWM(int pwmSpeed) where -255 <= pwmSpeed <= 255, positive
+ * values will cause the motor to turn CW and negative CCW.
+ * 
+ * A particular implementation of a driver has to extend this class coding
+ * the  writePWM method to behave as defined above.
+ * 
+ * Created by Javier Casado July 2016
+ * Licence CC BY
+ */
 #ifndef MotorDriver_h
 #define MotorDriver_h
 
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MotorDriver
+
+Provides an homogeneous interface to deal with a variety of pwm motor drivers in Arduino.
+
+All PWM motor drivers control the speed and direction of a DC motor using different number of
+pins. The aim of this class is to provide an homogeneous interface for an Arduino program to
+be abel to control the speed of a DC Motor.
+
+It contains just one method called writePWM(int pwmSpeed) where -255 <= pwmSpeed <= 255, positive
+values will cause the motor to turn CW and negative CCW.
+
+A particular implementation of a driver has to extend this class   coding
+the  writePWM method to behave as defined above.
